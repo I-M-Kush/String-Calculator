1f62b46dc90358200e9a72f96fdc9e46b12333ea
kush patel <kushpatel259000@gmail.com>
2022-09-05 08:12:51 +0530

    Allow Alphabet

--- String Calculator.py
+++ String Calculator.py
@@ -4,14 +4,17 @@ class StringCalculator:
         for x in Number:
             if x == 0:
                 sum=0
-            else:
-                sum=sum+int(x)
+            else :
+                 if x.isalpha():
+                    x=x.lower()
+                    sum =sum + int(str(ord(x)-96))
+                 else:
+                    sum=sum+int(x)
         print(sum)
             
 
 
-number=str(input("Enter number sepreted by Commm :"))
+number=str(input("Enter number sepreted by Comma :"))
 Numberlist=number.split(",")
 Sc=StringCalculator()
 Sc.add(Numberlist)
- 
